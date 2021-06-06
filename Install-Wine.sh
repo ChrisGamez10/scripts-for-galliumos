37b0ed3ba3fc5fe7f045443be1ff800637269da5
ChrisGamez10 <76846997+ChrisGamez10@users.noreply.github.com>
2021-06-06 18:14:00 +0000

    Create Install-Wine.sh

---
+++ Install-Wine.sh
@@ -0,0 +1,34 @@
+echo "Adding 32-bit architecture..."
+sudo dpkg --add-architecture i386 
+echo "Adding WineHQ repository key..."
+wget -nc https://dl.winehq.org/wine-builds/winehq.key
+sudo apt-key add winehq.key
+echo "Adding WineHQ repository..."
+sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main'
+echo "Updating packages..."
+sudo apt update
+echo "Adding FAudio repository key..."
+wget -nc https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/xUbuntu_18.04/Release.key
+sudo apt-key add Release.key
+echo "Adding FAudio repository..."
+sudo add-apt-repository 'deb https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/xUbuntu_18.04/ ./'
+echo "Updating packages (again)..."
+sudo apt update
+PS3="Which version of Wine would you like to install?: "
+
+select opt in Wine-Stable Wine-Development Wine-Staging; do
+
+case $opt in
+Wine-Stable)
+sudo apt install --install-recommends winehq-stable
+;;
+Wine-Development)
+sudo apt install --install-recommends winehq-devel
+;;
+Wine-Staging)
+sudo apt install --install-recommends winehq-staging
+;;
+*)
+echo "You have selected an invalid option. Please try again."
+esac
+done
